661314f1535e0e0cf8ed54f638e66d537519e2a0
jeff.lo <lo.jeffn@gmail.com>
2024-06-24 13:55:03 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Resume
+I had stumbled across the Cloud Resume Challenge (https://cloudresumechallenge.dev/) and thought this this would a neat challenge to keep my current skills in check however to also learn new skills along the way.
+
+This challenge has includes:
+
+- Creating a website from HTML and styled in CSS
+
+- Creating a counter and saving the counter to a database
+
+- Creating a backend integration, displaying the counter on the homepage
+
+- Creating a CI/CD with Github Actions and automating the infrastructure
+
+Tools Used: 
+- Azure
+- Python
+- HTML/CSS
---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitorCount();
+})
+
+const functionApiUrl = 'http://localhost:7071/api/main';
+const localFunctionApi = 'http://localhost:7071/api/main';
+
+const getVisitorCount = () => {
+    let count = 5;
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count =  response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
