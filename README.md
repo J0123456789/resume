a5c1765df5874342a1d7878c2b1941c961cf95ed
jeff.lo <lo.jeffn@gmail.com>
2024-06-25 07:34:18 -0500

    Untrack files

---
+++ README.md
@@ -0,0 +1,28 @@
+# Azure Cloud Resume
+I had stumbled across the Cloud Resume Challenge (https://cloudresumechallenge.dev/) and thought this this would a fun challenge and definitely a challenge it was!
+
+Feel free to check out my site at https://jefflo.me
+
+The challenge included:
+
+- Creating a resume written in HTML and styled with CSS.
+
+- Deploying the resume online as an Azure Stroage static website securely through https utilizing Azure CDN while pointing to a custom domain name to the Azure CDN endpoint.
+
+- Creating a visitor counter on the resume page using Javascript.
+
+- Utilizing Azure CosmosDB to retrieve and udpate the counter.
+
+- Creating an API using Azure Functions with an HTTP trigger written in Python.
+
+- Test the Python code and Azure Function.
+
+- Use Source control.
+
+- Create CI/CD Pipeline for both the backend and frontend.
+
+The tools I used for this were:
+- VSCode
+- Python
+- HTML/CSS
+- GitHub
---
+++ backend/api/counter/test_counter.py
@@ -0,0 +1,28 @@
+import os
+import json
+import requests
+from azure.functions import HttpRequest
+from function_app import main
+import pytest
+
+def test_function_app():
+    cosmos_db_connection_string = os.environ.get("COSMOSDB_CONNECTION_STRING")
+   # Check if cosmos_db_connection_string is set 
+    if not cosmos_db_connection_string:
+        raise ValueError("COSMOSDB_CONNECTION_STRING environment variable is not set.")
+
+    # Simulate an HTTP GET request
+    req = HttpRequest(method="GET", url="/api/main", body=None, headers={}, params={})
+    response = main(req)
+
+    # Assert the response status code
+    assert response.status_code == 200, "Expected HTTP status code 200 OK"
+
+    # Assert the response content type
+    assert response.headers["Content-Type"] == "application/json", "Expected Content-Type to be application/json"
+
+    # Assert the count has been incremented assuming count is greater than 0
+    response_body = json.loads(response.get_body())
+    assert "count" in response_body, "Response body should contain 'count'"
+    new_count = int(response_body["count"])
+    assert new_count > 0, "Count should be greater than 0"
